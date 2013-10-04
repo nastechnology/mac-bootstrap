120e8817aff0ebf0bd1c84f0f4891b2d86014a3b
Mark Myers <marcusmyers@gmail.com>
2013-10-04 10:57:28 -0400

    Created so user create is autologged in user

--- createUser.sh
+++ createUser.sh
@@ -2,7 +2,7 @@
 USERNAME=$1
 . /etc/rc.common
 dscl . create /Users/${USERNAME}
-dscl . create /Users/${USERNAME}  RealName "Basic User"
+dscl . create /Users/${USERNAME}  RealName "${USERNAME}"
 dscl . create /Users/${USERNAME}  hint "Default"
 dscl . create /Users/${USERNAME}  picture "/Library/User Pictures/Animals/Penguin.tif"
 dscl . passwd /Users/${USERNAME}  school
@@ -11,4 +11,7 @@ dscl . create /Users/${USERNAME}  PrimaryGroupID 20
 dscl . create /Users/${USERNAME}  UserShell /bin/bash
 dscl . create /Users/${USERNAME}  NFSHomeDirectory /Users/${USERNAME} 
 cp -R /System/Library/User\ Template/English.lproj /Users/${USERNAME} 
-chown -R ${USERNAME} :staff /Users/${USERNAME} 
+chown -R ${USERNAME}:staff /Users/${USERNAME} 
+
+defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser ${USERNAME}
+defaults write /Library/Preferences/com.apple.loginwindow autoLoginUID 575
